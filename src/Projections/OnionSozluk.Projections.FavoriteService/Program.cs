d521ab204230d130ee73a32a1ebfe908641d3551
kaangemalmaz <kaangemalmaz@gmail.com>
2023-01-15 18:27:30 +0300

    Project Folder Structure

---
+++ src/Projections/OnionSozluk.Projections.FavoriteService/Program.cs
@@ -0,0 +1,10 @@
+using OnionSozluk.Projections.FavoriteService;
+
+IHost host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Worker>();
+    })
+    .Build();
+
+await host.RunAsync();
